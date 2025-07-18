4b4c7b71561c4b5e45fc39f7980bf09f95641c0a
Mehmet <mehmetext68@gmail.com>
2025-07-18 22:10:54 +0300

    feat: add env control to useFactory in git.module

--- src/git/git.module.ts
+++ src/git/git.module.ts
@@ -3,12 +3,24 @@ import { GitResolver } from './git.resolver';
 import { GitService } from './git.service';
 import { GitProvider } from './providers/git.provider';
 import { GithubProvider } from './providers/github.provider';
+import { MockProvider } from './providers/mock.provider';
 
 @Module({
   providers: [
     GitResolver,
     GitService,
-    { provide: GitProvider, useClass: GithubProvider },
+    {
+      provide: GitProvider,
+      useFactory: () => {
+        if (!process.env.NODE_ENV) {
+          throw new Error('NODE_ENV is not set');
+        }
+        if (process.env.NODE_ENV === 'production') {
+          return new GithubProvider();
+        }
+        return new MockProvider();
+      },
+    },
   ],
 })
 export class GitModule {}
